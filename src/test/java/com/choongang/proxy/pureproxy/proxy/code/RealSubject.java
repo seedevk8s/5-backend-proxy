33f892dd0c6555d9eae8aa3e1aff4296e86e455c
seedevk8s <seedevk8s@gmail.com>
2025-05-22 20:34:02 +0900

    인터페이스와 그 구현 클래스 추가

---
+++ src/test/java/com/choongang/proxy/pureproxy/proxy/code/RealSubject.java
@@ -0,0 +1,21 @@
+package com.choongang.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RealSubject implements Subject {
+    @Override
+    public String operation() {
+        log.info("실제 객체 호출");
+        sleep(1000);    // Simulate a delay. 데이터 조회를 시뮬레이션 하기 위해 1초 쉬도록 했다
+        return "data";
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
